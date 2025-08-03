a0ca14093cbf86e0ffadf82f1ca41293df427b79
Deshanth Vishvalingam <desh0sevn@gmail.com>
2025-08-03 16:55:09 +0530

    Initial interface

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+    socket.on('join', (userId) => {
+        socket.join(userId);
+    });
+
+
+    socket.on('send_message', (data) => {
+        // Broadcast to recipient
+        io.to(data.to).emit('receive_message', data);
+    });
+
+    // File upload via base64 (for small files/images)
+    socket.on('send_file', (data) => {
+        // data: { from, to, filename, filedata (base64), type }
+        io.to(data.to).emit('receive_message', {
+            from: data.from,
+            to: data.to,
+            content: `<a href='${data.filedata}' download='${data.filename}'>📎 ${data.filename}</a>`,
+            type: 'file',
+            filename: data.filename
+        });
+    });
+
+    socket.on('typing', (data) => {
+        io.to(data.to).emit('typing', data);
+    });
+});
+
+server.listen(8080, () => {
+    console.log('Server running on http://localhost:8080');
+});
